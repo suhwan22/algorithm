0300caa5a8977ad1dc4169db4b6ae7f77c264af1
suhwan <tnghkswkd123@naver.com>
2024-11-15 21:40:42 +0900

    [feat] Baekjoon#14501 solved with c++

---
+++ Baekjoon/0x0C_DP/14501/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+pair<int, int> schedule[16] = {};
+bool used[16] = {};
+int n;
+int max = 0;
+
+void dfs(int idx, int sum);
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+    cin >> schedule[i].first >> schedule[i].second;
+
+  for (int i = 1; i <= n; i++)
+  {
+    if (i + schedule[i].first > n + 1) continue;
+    dfs(i, schedule[i].second);
+  }
+
+  cout << ::max;
+  return 0;
+}
+
+void dfs(int idx, int sum)
+{
+  if (::max < sum)
+    ::max = sum;
+  for (int i = idx + 1; i <= n; i++)
+  {
+    if (i < idx + schedule[idx].first) continue;
+    if (i + schedule[i].first > n + 1) continue;
+    dfs(i, sum + schedule[i].second);
+  }
+}
