d08d91c05dda31a288cc73b11eeded1aaf7ecb1b
suhwan <tnghkswkd123@naver.com>
2024-12-26 18:55:18 +0900

    [feat] Baekjoon#9465 solved with c++

---
+++ Baekjoon/0x0C_DP/9465/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int t, n;
+int board[2][100000] = {};
+int D[3][100000] = {};
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> t;
+	while (t--)
+	{
+		cin >> n;
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < n; j++)
+				cin >> board[i][j];
+		}
+		D[0][0] = board[0][0];
+		D[1][0] = board[1][0];
+		D[2][0] = 0;
+		for (int i = 1; i < n; i++)
+		{
+			D[0][i] = board[0][i] + max(D[1][i-1], D[2][i-1]);
+			D[1][i] = board[1][i] + max(D[0][i-1], D[2][i-1]);
+			D[2][i] = max(D[0][i-1], D[1][i-1]);
+		}
+		cout << max(max(D[0][n-1], D[1][n-1]), D[2][n-1]) << "\n";
+	}
+
+	return 0;
+}
