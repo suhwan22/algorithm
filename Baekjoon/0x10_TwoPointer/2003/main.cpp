d81fca71eb05998c70dc19edbfe7b3307f5078ba
suhwan <tnghkswkd123@naver.com>
2025-01-03 22:11:13 +0900

    [feat] Baekjoon#2003 solved with c++

---
+++ Baekjoon/0x10_TwoPointer/2003/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10001] = {};
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	int st = 0, en = 0;
+	long long sum = 0;
+	int cnt = 0;
+	while (st <= en && en <= n)
+	{
+		if (sum > m)
+		{
+			sum -= arr[st];
+			st++;
+		}
+		else if (sum <= m)
+		{
+			if (sum == m)
+				cnt++;
+			sum += arr[en];
+			en++;
+		}
+	}
+	cout << cnt;
+
+	return 0;
+}
