b543aefdaf000050a93c4837dde333576fe11bc1
suhwan <tnghkswkd123@naver.com>
2025-02-08 23:02:01 +0900

    [feat] Baekjoon#7570 solved with c++

--- Baekjoon/0x0D_Greedy/7570/main.cpp
+++ Baekjoon/0x0D_Greedy/7570/main.cpp
@@ -3,32 +3,32 @@
  * Email       : ksuhwan22@gmail.com
  * GitHub      : https://github.com/suhwan22
  * Additional  : https://guiltless-break-8cc.notion.site/boj
- * Created     : 2025-02-08 16:35:14
- * Updated     : 2025-02-08 17:12:36
+ * Created     : 2025-02-08 22:27:17
+ * Updated     : 2025-02-08 23:01:31
  */
 
 #include <iostream>
-#include <set>
 
 using namespace std;
 
 int n;
-set<int> LIS;
+int D[1000001];
+int seq[1000001];
 
 int main()
 {
 	cin.tie(0)->sync_with_stdio(0);
 	cin >> n;
-	int num;
-	for (int i = 0; i < n; i++)
+	int num, max = 0;
+	for (int i = 1; i <= n; i++)
 	{
 		cin >> num;
-		auto it = LIS.lower_bound(num);
-		if (it != LIS.end())
-			LIS.erase(it);
-		LIS.insert(num);
+		seq[num] = i;
+		D[i] = D[seq[num - 1]] + 1;
+		if (max < D[i])
+			max = D[i];
 	}
-	cout << n - LIS.size();
+	cout << n - max;
 
 	return 0;
 }
