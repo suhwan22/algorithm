e3010a8c30b0867b8b758cd7413cd1bab6970715
suhwan <tnghkswkd123@naver.com>
2025-02-09 18:00:15 +0900

    [refac] Baekjoon#7570
    index를 저장하던 seq[]을 제거하고 입력 num을 D[]에 index로 사용해서
    최적화 함

--- Baekjoon/0x0D_Greedy/7570/main.cpp
+++ Baekjoon/0x0D_Greedy/7570/main.cpp
@@ -4,7 +4,7 @@
  * GitHub      : https://github.com/suhwan22
  * Additional  : https://guiltless-break-8cc.notion.site/boj
  * Created     : 2025-02-08 22:27:17
- * Updated     : 2025-02-08 23:01:31
+ * Updated     : 2025-02-09 17:56:33
  */
 
 #include <iostream>
@@ -13,7 +13,6 @@ using namespace std;
 
 int n;
 int D[1000001];
-int seq[1000001];
 
 int main()
 {
@@ -23,10 +22,9 @@ int main()
 	for (int i = 1; i <= n; i++)
 	{
 		cin >> num;
-		seq[num] = i;
-		D[i] = D[seq[num - 1]] + 1;
-		if (max < D[i])
-			max = D[i];
+		D[num] = D[num-1] + 1;
+		if (max < D[num])
+			max = D[num];
 	}
 	cout << n - max;
 
