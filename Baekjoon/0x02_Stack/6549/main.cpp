b2be7bbde832af8c368c3612887cfb8146cb5abc
suhwan <tnghkswkd123@naver.com>
2025-04-02 23:05:29 +0900

    [refac] Baekjoon#6549 refac
    1. while문 반복을 제거함
    2. 입력을 배열로 받지 않고 그때 그때 처리하도록 변경

--- Baekjoon/0x02_Stack/6549/main.cpp
+++ Baekjoon/0x02_Stack/6549/main.cpp
@@ -1,75 +1,47 @@
+/*
+ * Author      : suhwan22
+ * Email       : ksuhwan22@gmail.com
+ * GitHub      : https://github.com/suhwan22
+ * Additional  : https://guiltless-break-8cc.notion.site/boj-6549
+ * Created     : 2025-04-02 19:50:22
+ * Updated     : 2025-04-02 21:26:26
+ */
+
 #include <iostream>
 #include <stack>
+#include <map>
 
 using namespace std;
 
-void printStack(stack<pair<long long, long long>> s)
-{
-  cout << "----stack----\n";
-  for (;!s.empty();s.pop())
-  {
-    cout << "" << s.top().first << ": " << s.top().second << "\n";
-  }
-  cout << "-------------\n";
-}
+int n;
+stack<pair<long long, int>> s;
 
 int main()
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  int n;
-  long long h;
-  long long cnt;
-  long long temp;
-  long long max = 0;
-
+	cin.tie(0)->sync_with_stdio(0);
   while (1)
   {
-    stack<pair<long long, long long>> s;
     cin >> n;
-    max = 0;
+    long long h, ans = 0;
     if (n == 0)
       break;
-    //run
-    while (n--)
+    for (int i = 0; i <= n; i++)
     {
-      cin >> h;
-      cnt = 0;
-      while (!s.empty() && s.top().first > h)
+      if (i < n)
+        cin >> h;
+      pair<long long, int> cur = {0, i};
+      while (!s.empty() && (i == n || h < s.top().first))
       {
-        // pop 하면서 정리
-        if (max < s.top().first * (s.top().second + cnt))
-          max = s.top().first * (s.top().second + cnt);
-        cnt += s.top().second;
+        cur = s.top();
         s.pop();
+        if (ans < cur.first * (i - cur.second))
+          ans = cur.first * (i - cur.second);
       }
-      if (!s.empty() && s.top().first == h)
-      {
-        // pop 다했는데 같은게 남은 경우
-        cnt += s.top().second;
-        s.pop();
-        s.push({h, cnt + 1});
-      }
-      else
-      {
-        // pop 다 했는데 그냥 다른경우(일반적인 경우)
-        // 쌓인 cnt 더해주고 나는 새로 하나 쌓고
-        s.push({h, cnt + 1});
-      }
-      // printStack(s);
+      if (i != n && h != 0 && (s.empty() || s.top().first < h))
+        s.push({h, cur.second});
     }
-    cnt = 0;
-    while (!s.empty())
-    {
-      if (s.top().first * (s.top().second + cnt) > max)
-        max = s.top().first * (s.top().second + cnt);
-      cnt += s.top().second;
-      s.pop();
-    }
-  
-    cout << max << "\n";
+    cout << ans << "\n";
   }
 
-  return 0;
+	return 0;
 }
