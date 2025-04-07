99cb33fabb32b59673b358384a528d5a1c1172a3
suhwan <tnghkswkd123@naver.com>
2025-04-07 14:42:52 +0900

    [feat] Baekjoon#11718 solved with c++

---
+++ Baekjoon/extra/11718/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author      : suhwan22
+ * Email       : ksuhwan22@gmail.com
+ * GitHub      : https://github.com/suhwan22
+ * Additional  : https://guiltless-break-8cc.notion.site/boj
+ * Created     : 2025-04-07 14:41:22
+ * Updated     : 2025-04-07 14:42:45
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	string input;
+	while (getline(cin, input))
+		cout << input << "\n";
+
+	return 0;
+}
