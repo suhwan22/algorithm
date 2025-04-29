be9af0b31028584964c2a21a6205bfaca9ba0baa
suhwan <tnghkswkd123@naver.com>
2025-04-30 01:05:22 +0900

    [feat] Baekjoon#15829 solved with c++

---
+++ Baekjoon/extra/15829/main.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author      : suhwan22
+ * Email       : ksuhwan22@gmail.com
+ * GitHub      : https://github.com/suhwan22
+ * Additional  : https://guiltless-break-8cc.notion.site/boj
+ * Created     : 2025-04-30 00:55:32
+ * Updated     : 2025-04-30 00:55:32
+ */
+
+#include <iostream>
+
+using namespace std;
+
+long long ans;
+
+long long mypow(long long n, int k);
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int len;
+	cin >> len;
+	string input;
+	cin >> input;
+	for (int i = 0; i < len; i++)
+	{
+		ans = (ans + (input[i] - 'a' + 1) * mypow(31, i)) % 1234567891;
+		cout << i << ": " << ans << "\n";
+	}
+	cout << ans;
+
+	return 0;
+}
+
+long long mypow(long long n, int k)
+{
+	long long ret = 1;
+	for (int i = 0; i < k; i++)
+		ret = (ret * n) % 1234567891;
+	return ret;
+}
